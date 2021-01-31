f808dbcff3aaaa0b470b69bcd121f91bed3a12fc
Sebastián Uncos <suncos.empleo@gmail.com>
2021-01-31 01:20:12 -0300

    4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»

---
+++ js/ejercicio4.js
@@ -0,0 +1,6 @@
+/*
+4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+*/
+
+let nombreUsuario = prompt("Por favor ingrese el nombre de usuario:");
+document.write("Hola " + nombreUsuario + "!");
