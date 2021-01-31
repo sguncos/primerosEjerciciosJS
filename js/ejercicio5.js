0b7336b16667ddd0caa067e731eedd3ef04d524b
Sebastián Uncos <suncos.empleo@gmail.com>
2021-01-31 01:34:10 -0300

    se creo el archivo ejercio5.js, se lo vinculo al html, se resolvio el ejercicio5

---
+++ js/ejercicio5.js
@@ -0,0 +1,7 @@
+/*
+5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+*/
+
+let primerNumero = parseInt(prompt("Por favor ingrese el primer número:"));
+let segundoNumero = parseInt(prompt("Por favor ingrese en segundo número"));
+document.write("El resultado de la suma de los números ingresados es: " + (primerNumero + segundoNumero));
